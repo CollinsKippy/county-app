8d03be450efde023755819b03af0bc2b5e8ac6c5
CK <collins@analence.com>
2022-10-22 13:23:35 +0300

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Success, my people!' });
+});
+
+app.get('/api/counties', (req, res) => {
+  return res.status(200).json({ message: 'Counties List' });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
