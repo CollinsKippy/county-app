386322432e2b52d0b3c8bed080f0c54534b373c0
CK <collins@analence.com>
2022-10-25 10:46:06 +0300

    updated lake routes

--- backend/routes/lakeRoutes.js
+++ backend/routes/lakeRoutes.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getLakes,
+  getLake,
+  createLake,
+  updateLake,
+  deleteLake,
+} = require('../controllers/lakeController');
 
+router.get('/', getLakes);
+router.get('/:id', getLake);
+router.put('/:id', updateLake);
+router.post('/', createLake);
+router.delete('/:id', deleteLake);
 
 module.exports = router;
